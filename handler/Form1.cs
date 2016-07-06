765d460cd46ba258d35b26caada9b0e85f657a30
sw <suishanwen@icloud.com>
2016-07-06 09:08:22 +0800

    build

--- handler/Form1.cs
+++ handler/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using controller.util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,9 +12,52 @@ namespace handler
 {
     public partial class Form1 : Form
     {
+        private string pathShare;
+        private int no;
+        private int delay;
+        private int overTime;
+
+
         public Form1()
         {
             InitializeComponent();
+            if (File.Exists(@".\handler.ini"))
+            {
+                pathShare = IniReadWriter.ReadIniKeys("Command", "gongxiang", "./handler.ini");
+                no = int.Parse(IniReadWriter.ReadIniKeys("Command", "bianhao", "./handler.ini"));
+                try
+                {
+                    delay = int.Parse(IniReadWriter.ReadIniKeys("Command", "yanchi", "./handler.ini"));
+                }
+                catch (Exception)
+                {
+                    delay = 0;
+                }
+                textBox1.Text =  no.ToString();
+                overTime = int.Parse(IniReadWriter.ReadIniKeys("Command", "cishu", pathShare+"/CF.ini"));
+
+            }
+            else
+            {
+                MessageBox.Show("请设置handler.ini");
+                Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            IniReadWriter.WriteIniKeys("Command", "bianhao", textBox1.Text, "./handler.ini");
+        }
+
+
+        public void _main()
+        {
+
         }
     }
 }
---
+++ handler/util/SwitchUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using controller.util;
+using System.Windows.Forms;
+
+namespace controller.util
+{
+    class SwitchUtil
+    {
+        public static void swichVm(string vm1,string vm2,TextBox textBox,string customPath, string taskName,string pathShare)
+        {
+            if (StringUtil.isEmpty(textBox.Text))
+            {
+                if (!StringUtil.isEmpty(vm1) && !StringUtil.isEmpty(vm2))
+                {
+                    for (int i = int.Parse(vm1); i <= int.Parse(vm2); i++)
+                    {
+                        IniReadWriter.WriteIniKeys("Command", "CacheMemory" + i, "", pathShare + "/TaskPlus.ini");
+                        IniReadWriter.WriteIniKeys("Command", "CustomPath" + i, customPath, pathShare + "/TaskPlus.ini");
+                        IniReadWriter.WriteIniKeys("Command", "TaskName" + i, taskName, pathShare + "/Task.ini");
+                        IniReadWriter.WriteIniKeys("Command", "TaskChange" + i, "1", pathShare + "/Task.ini");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("虚拟机不能为空！");
+                }
+
+            }
+            else
+            {
+                IniReadWriter.WriteIniKeys("Command", "CacheMemory" + textBox.Text, "", pathShare + "/TaskPlus.ini");
+                IniReadWriter.WriteIniKeys("Command", "CustomPath" + textBox.Text, customPath, pathShare + "/TaskPlus.ini");
+                IniReadWriter.WriteIniKeys("Command", "TaskName" + textBox.Text, taskName, pathShare + "/Task.ini");
+                IniReadWriter.WriteIniKeys("Command", "TaskChange" + textBox.Text, "1", pathShare + "/Task.ini");
+                textBox.Text = "";
+            }
+        }
+    }
+}
