a6b0e4206dfbe9e86a7d1e04270810e5f12e9c79
sw <suishanwen@icloud.com>
2016-09-14 16:23:05 +0800

    add apiSetComputerNameEx

---
+++ handler/util/Computer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace handler.util
+{
+    class Computer
+    {
+        [DllImport("kernel32.dll", EntryPoint = "SetComputerNameEx")]
+        public static extern int apiSetComputerNameEx(int type, string lpComputerName);
+    }
+}
