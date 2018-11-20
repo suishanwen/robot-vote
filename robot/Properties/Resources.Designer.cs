bfdfc63c4f8557590a7efb05e02b12a991b84554
sw <suishanwen@icloud.com>
2018-11-20 13:39:02 +0800

    U

--- robot/Properties/Resources.Designer.cs
+++ robot/Properties/Resources.Designer.cs
@@ -36,7 +36,7 @@ namespace robot.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("handler.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("robot.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
